0519a4d7b2e53f161f88a7572a3d337ab8767e7b
Sagiri-blyat <58057821+Sagiri-blyat@users.noreply.github.com>
2020-07-18 18:31:29 +0800

    Add files via upload

--- proj/Forms.py
+++ proj/Forms.py
@@ -3,10 +3,6 @@ from wtforms import Form, StringField, DateField, SelectField, TextAreaField,\
 from wtforms.validators import InputRequired, Email
 from wtforms.fields.html5 import EmailField
 
-from django import forms
-from wtforms_components import DateRange
-# from datetime import date, datetime
-
 
 class RegistrationForm(Form):
     first_name = StringField('First Name', [validators.Length(min=2)], render_kw={"placeholder": "John"})
@@ -14,21 +10,13 @@ class RegistrationForm(Form):
     email = EmailField("Email",  validators=[InputRequired("Please enter your email address."), Email("Please enter your email address.")],render_kw={"placeholder": "test@test"})
     password = PasswordField('Password', [validators.DataRequired()], render_kw={"placeholder": "Password: "})
     confirm = PasswordField('Confirm', [validators.DataRequired()], render_kw={"placeholder": "Confirm Password:"})
-
-
-    # password = PasswordField('New Password', [
-    #     validators.DataRequired(),
-    #     validators.EqualTo('confirm', message='Invalid Password. Passwords must match.')
-    # ], render_kw={"placeholder": "Password"})
-    # confirm = PasswordField('Repeat Password',render_kw={"placeholder": "Confirm Password"})
     accept_tos = BooleanField('', [validators.DataRequired()] )
 
-
-# class LoginForm(Form):
-#     email = EmailField("Email",  validators=[InputRequired("Please enter your email address."), Email("Please enter your email address.")],render_kw={"placeholder": "test@test"})
-#     password = PasswordField('Password', [validators.DataRequired()], render_kw={"placeholder": "Password"})
 
 class LoginForm(Form):
     email = StringField("Email",render_kw={"placeholder": "test@test.test"})
     password = PasswordField('Password', [validators.DataRequired()], render_kw={"placeholder": "Password"})
 
+
+class ResetForm(Form):
+    email = EmailField("Email",  validators=[InputRequired("Please enter your email address."), Email("Please enter your email address.")],render_kw={"placeholder": "test@test"})
